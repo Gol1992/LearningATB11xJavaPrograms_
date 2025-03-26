115445601bb2ef2d2a01808f9d97bb0819d4087b
Gol1992 <garima076singh@gmail.com>
2025-03-26 15:09:29 +0530

    another commit(26thMarch)

--- src/Lab001_HelloWorldProgram.java
+++ src/Lab001_HelloWorldProgram.java
@@ -4,5 +4,6 @@ public class Lab001_HelloWorldProgram {
     public static  void main(String[] args) {
         System.out.println("HelloJava");
         System.out.println("HelloJava");
+        System.out.println("Hello India");
     }
 }
