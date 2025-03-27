bb98060e59234d8489b7e5ed736ed1483b40c452
Gol1992 <garima076singh@gmail.com>
2025-03-27 11:30:10 +0530

    27th Draft Commit 1

--- src/ex_Strings/Strings_Immutable.java
+++ src/ex_Strings/Strings_Immutable.java
@@ -1,4 +1,12 @@
 package ex_Strings;
 
 public class Strings_Immutable {
+    public static void main(String[] args) {
+
+        String name = "Garima";
+        name = "Genius" + " " + name;
+
+        System.out.println(name);
+
+    }
 }
