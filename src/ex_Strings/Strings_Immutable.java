2e0f9fee0086d86cd4fd86a63507ba06e6cb58a2
Gol1992 <garima076singh@gmail.com>
2025-03-27 13:31:33 +0530

    27th_Strings and Functions commit

--- src/ex_Strings/Strings_Immutable.java
+++ src/ex_Strings/Strings_Immutable.java
@@ -3,10 +3,15 @@ package ex_Strings;
 public class Strings_Immutable {
     public static void main(String[] args) {
 
+        // immutable example 1
         String name = "Garima";
         name = "Genius" + " " + name;
 
         System.out.println(name);
 
+        //immutable example 2
+        String s1 = "Hello";
+        s1 = s1.concat(" "+ "Java");
+        System.out.println(s1);
     }
 }
